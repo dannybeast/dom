85e72fc9450ae38f99b1fca9d97c31282e321107
dannybeast <dannybeastrus@gmail.com>
2022-07-04 23:03:00 +0300

    First

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import './js/modules/main';
+import './js/modules/svg';
+
+// SCSS
+import "./assets/scss/app.scss";
+window.JQuery = $
+
+$(document).ready(function () {
+
+  $('body').addClass('is-loaded')
+})
---
+++ src/js/modules/main.js
@@ -0,0 +1,12 @@
+//import './svg';
+
+// Modules
+import $ from "jquery";
+import App from "./app";
+import Fancy from "./fancybox";
+$(document).ready(function () {
+
+  Fancy.init();
+
+  App.currentYear()
+})
