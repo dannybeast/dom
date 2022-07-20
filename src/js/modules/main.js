2df4cff529707f7a7e67cb6b0ae909b55b15d7b1
dannybeast <dannybeastrus@gmail.com>
2022-07-20 20:16:06 +0300

    Feat: Доработки

--- src/js/modules/main.js
+++ src/js/modules/main.js
@@ -4,9 +4,11 @@
 import $ from "jquery";
 import App from "./app";
 //import Fancy from "./fancybox";
+import Select from "./select";
+
 $(document).ready(function () {
  // Fancy.init();
-
+  Select.init();
   App.anchors();
   App.hideCookieAlert();
 })
