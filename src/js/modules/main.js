ce2dab78b626b92201b49f70f17e7f35d8d0954c
dannybeast <dannybeastrus@gmail.com>
2022-07-21 17:01:32 +0300

    Feat: Валидация

--- src/js/modules/main.js
+++ src/js/modules/main.js
@@ -3,12 +3,22 @@
 // Modules
 import $ from "jquery";
 import App from "./app";
-//import Fancy from "./fancybox";
+import Fancy from "./fancybox";
+import Masks from "./inputmask";
 import Select from "./select";
+import Validation from '../forms/validation'
+
+// Global
+window.Masks = Masks
+window.Select = Select
+window.App = App
+window.Fancy = Fancy
 
 $(document).ready(function () {
- // Fancy.init();
+  Fancy.init();
   Select.init();
+  Validation();
+  Masks.maskPhone();
   App.anchors();
   App.toggleSorting();
   App.hideCookieAlert();
