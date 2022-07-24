ef9280a996b11972e7ea39eb4d5e10598208f6b4
dannybeast <dannybeastrus@gmail.com>
2022-07-24 20:25:47 +0300

    Feat: Filters

--- src/js/modules/main.js
+++ src/js/modules/main.js
@@ -5,19 +5,19 @@ import $ from "jquery";
 import App from "./app";
 import Fancy from "./fancybox";
 import Masks from "./inputmask";
-import Select from "./select";
+//import Select from "./select";
 import Validation from '../forms/validation'
 import sliderCatalogItem from "../sliders/sliderCatalogItem";
 
 // Global
 window.Masks = Masks
-window.Select = Select
+//window.Select = Select
 window.App = App
 window.Fancy = Fancy
 
 $(document).ready(function () {
   Fancy.init();
-  Select.init();
+ //Select.init();
   Validation();
   Masks.maskPhone();
   App.anchors();
--- src/js/modules/select.js
+++ src/js/modules/select.js
@@ -3,13 +3,23 @@ import "select2/dist/js/select2";
 class Select{
 
   init() {
+
     $('.js-select').each(function(){
       let placeholder = $(this).data('placeholder')
+      let minimumResultsForSearch = $(this).data('search') ? 1 : -1
       $(this).select2({
-        minimumResultsForSearch: -1,
-        placeholder
+       minimumResultsForSearch,
+       placeholder,
+           "language": {
+           "noResults": function() {
+            return "Не найдено";
+           }
+        },
+        dropdownParent: $(this).parent()
       });
-    })
+    }).on('select2:open', function (e) {
+     $( e.target).parent().find('.select2-search__field').attr('placeholder','Поиск');
+   });
   }
 
 }
