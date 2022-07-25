db4deeef41200be1940658f1c6357e7a39826dda
dannybeast <dannybeastrus@gmail.com>
2022-07-25 12:13:54 +0300

    Feat: Refactor range slider

--- src/js/modules/rangeSlider.js
+++ src/js/modules/rangeSlider.js
@@ -2,28 +2,19 @@ import "jquery-ui/ui/widgets/slider";
 import "jquery-ui-touch-punch";
 
 export default function(){
-  
-$('.range-slider-wrapper').each(function(){
+  let wrapper = '.range-slider-wrapper',
+      rangeMin = '.js-range-min',
+      rangeMax = '.js-range-max',
+      rangeSlider = '.range-slider'
+      
+$(wrapper).each(function(){
   let min = $(this).data('min')
   let max = $(this).data('max')
   let step = $(this).data('step')
-  let minVal = $(this).find('.js-range-min').val();
-  let maxVal = $(this).find('.js-range-max').val();
-  let that = $(this)
-  let slider = $(this).find('.range-slider')
+  let minVal = $(this).find(rangeMin).val();
+  let maxVal = $(this).find(rangeMax).val();
+  let slider = $(this).find(rangeSlider)
   
-  $(this).append(`
-  <div class="range-slider-wrapper__bottom">
-    <p class="range-slider-wrapper__price"> 
-     ${min}
-    </p>
-    <p class="range-slider-wrapper__price ">
-      ${max}
-    </p>
-  </div>`)
-
-
-
   slider.slider({
     range: true,
     min: min,
@@ -31,34 +22,24 @@ $('.range-slider-wrapper').each(function(){
     step:step,
     values: [minVal, maxVal],
     slide: function(event, ui) {
-      let $parent = $(event.target).closest('.range-slider-wrapper')
-      $parent.find('.js-range-min').val(ui.values[0]);
-      $parent.find('.js-range-max').val(ui.values[1]);
+      let $parent = $(event.target).closest(wrapper)
+      $parent.find(rangeMin).val(ui.values[0]);
+      $parent.find(rangeMax).val(ui.values[1]);
     },
     create: function(event, ui) {
-     let $parent = $(event.target).closest('.range-slider-wrapper')
+     let $parent = $(event.target).closest(wrapper)
 
-
-     $parent.find('.js-range-min').keyup(function(){
+     $parent.find(rangeMin).keyup(function(){
       minVal = $(this).val();
       slider.slider( "option",'values',[minVal,maxVal]);
-      that.find('.range-min').text(minVal);
-      that.find('.range-max').text(maxVal);
-    })
-  
-    $parent.find('.js-range-max').keyup(function(){
-      maxVal = $(this).val()
-      slider.slider( "option",'values',[minVal,maxVal]);
-      that.find('.range-min').text(minVal);
-      that.find('.range-max').text(maxVal);
-    })
-  
+     })
+   
+     $parent.find(rangeMax).keyup(function(){
+       maxVal = $(this).val()
+       slider.slider( "option",'values',[minVal,maxVal]);
+     })
    }
   });
-
-  $(this).find('.ui-slider-handle').eq(0).append(`<span class="range-min">${minVal}</span>`)
-  $(this).find('.ui-slider-handle').eq(1).append(`<span class="range-max">${maxVal}</span>`)
-
 })
 
 }
